899edbb4b542bab6a434a182b0dc581c1c1cd0b3
Vadim Leontiev <leontiev@greensight.ru>
2019-07-22 22:26:47 +0300

    Fix socket initialization problems and non-existent http_code

--- src/Checks/Custom/HttpsRedirect.php
+++ src/Checks/Custom/HttpsRedirect.php
@@ -3,7 +3,6 @@
 namespace Arrilot\BitrixSystemCheck\Checks\Custom;
 
 use Arrilot\BitrixSystemCheck\Checks\Check;
-use RuntimeException;
 
 class HttpsRedirect extends Check
 {
@@ -38,21 +37,22 @@ class HttpsRedirect extends Check
     {
         $url = str_replace('https://', 'http://', $this->mainPage);
         $info = $this->getCurlInfo($url, $this->basicAuth);
+
         if (is_null($info)) {
             return false;
         }
 
-        if (is_null($info)) {
+        if (!isset($info['http_code'])) {
             return false;
         }
 
         if ($info['http_code'] !== 301 && $info['http_code'] !== 302) {
-            $this->logError('При curl запросе к '. $url . ' получен код ответа ' . $info['http_code'] . ' вместо 301/302');
+            $this->logError('При curl запросе к ' . $url . ' получен код ответа ' . $info['http_code'] . ' вместо 301/302');
             return false;
         }
 
         if (rtrim($info['redirect_url'], '/') !== rtrim($this->mainPage, '/')) {
-            $this->logError('При curl запросе к '. $url . ' получен редирект на  ' . $info['redirect_url'] . ' вместо ' . $this->mainPage);
+            $this->logError('При curl запросе к ' . $url . ' получен редирект на  ' . $info['redirect_url'] . ' вместо ' . $this->mainPage);
             return false;
         }
 
